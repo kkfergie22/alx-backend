905b73fd7065202ed6d4c4c6ce023058b29c544b
Christian Koranteng <korantengchristian@gmail.com>
2023-04-20 05:49:23 +0000

    Fix 3

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -14,6 +14,7 @@ class Server:
 
     def __init__(self):
         self.__dataset = None
+        self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
         """Cached dataset
@@ -25,6 +26,18 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """
+        Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
 
     def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
         """Return a tuple of size two containing a start index and an end
