e02f57d8a1dc587b262a11acdec4d41bae893cde
Christian Koranteng <korantengchristian@gmail.com>
2023-04-25 16:02:50 +0000

    Update permissions

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -2,12 +2,6 @@
 """
 This module implements a Flask application with localization support
 using Flask-Babel.
-
-The application uses the Babel extension to support internationalization (i18n)
-and localization (l10n) of the content displayed in the templates.
-It allows the user to select their preferred language
-through the Accept-Language HTTP header or a query parameter in the URL.
-The application is configured to support two languages: English and French.
 """
 
 from typing import Tuple
@@ -18,15 +12,6 @@ from flask_babel import _
 
 
 class Config(object):
-    """
-    This class defines the configuration parameters for the Flask application.
-
-    Attributes:
-        LANGUAGES (list): A list of available languages supported
-        by the application.
-        DEFAULT_LOCALE (str): The default locale for the application.
-        DEFAULT_TIMEZONE (str): The default timezone for the application.
-    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -39,16 +24,6 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    """
-    This function determines the user's preferred locale for the application.
-
-    It tries to find the best match between the available languages and
-    the user's preferences,either through the Accept-Language HTTP header
-    or a query parameter in the URL.
-
-    Returns:
-        The preferred locale for the application as a string.
-    """
     supported_locales = app.config['LANGUAGES']
     locale = request.args.get('locale')
     if locale in supported_locales:
@@ -58,15 +33,6 @@ def get_locale() -> str:
 
 @app.route('/')
 def index() -> Tuple[str, int]:
-    """
-    This function displays the index page of the application.
-
-    It renders the '3-index.html' template using the
-    default language specified in the application configuration.
-
-    Returns:
-        A tuple containing the rendered HTML template.
-    """
     title = _('home_title')
     header = _('home_header')
     return render_template('4-index.html', title=title,
