333e0ce2b292b2fb0d0cc0ab068451e82b00179c
Christian Koranteng <korantengchristian@gmail.com>
2023-04-25 12:21:17 +0000

    Change permissions

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index() -> str:
+    """
+    This function is the view function for the root route of the Flask app.
+    It renders an HTML template named '0-index.html'
+    and returns it as a string.
+    """
     return render_template('0-index.html')
 
 
